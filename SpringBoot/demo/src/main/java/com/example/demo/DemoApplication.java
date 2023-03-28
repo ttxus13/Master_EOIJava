50a21d3820775471e7f1f5e09ef90df211e646d5
Ttxus <ttxus13@gmail.com>
2023-03-28 19:12:28 +0200

    Reorganization

--- SpringBoot/demo/src/main/java/com/example/demo/DemoApplication.java
+++ SpringBoot/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,27 +1,64 @@
 package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
+
+
+/**
+
+ * La Clase MySpringBootApplication, clase padre de la aplicaci&oacute;n SpringBoot
+
+ */
+
 @SpringBootApplication
+
 @RestController
+
 public class DemoApplication {
 
+
+
+	/**
+
+	 * The entry point of application.
+
+	 *
+
+	 * @param args the input arguments
+
+	 */
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
 	}
 
 
-	@RequestMapping("/hola")
-	String hellow() {
-		return "Hellooooou World";
+
+	/**
+
+	 * Hello main  string.
+
+	 *
+
+	 * @return  el string por defecto "Main Class" para testear la aplicación
+
+	 */
+
+	@GetMapping("/")
+
+	String helloMain() {
+
+		return "Main class";
+
 	}
-	@RequestMapping("/gif")
-	String gif() {
-		return "<IMG SRC=\"https://educacion30.b-cdn.net/wp-content/uploads/2019/06/homer.gif\">";
-	}
+
+
 
 }
-
